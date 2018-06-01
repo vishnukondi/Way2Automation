2745a6ddc0e5d8ec112368caef038a8dead31405
vishnukondi <vishnuvardhankondi@gmail.com>
2018-06-01 18:00:12 +0530

    Update AppTesting.java

--- src/test/java/AppTest/AppTesting.java
+++ src/test/java/AppTest/AppTesting.java
@@ -4,7 +4,7 @@ import org.testng.annotations.Test;
 
 import DriverScript.DriverScript;
 
-public class AppTesting {
+public class AppTest {
 	
 	@Test
 	public void startAppTest() throws Throwable {
